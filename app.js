3b7376399c7d84d627518828ba3c701639925559
Alexandre Uglar <acuglar@gmail.com>
2022-03-12 03:30:16 -0300

    Add getDocs to insert gamesList

--- app.js
+++ app.js
@@ -14,5 +14,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 
+getDocs(collection(db, 'games'))
+  .then(querySnapshot => {
+    // querySnapshot.docs.forEach(doc => console.log(doc.data())). método builtin js
+    // querySnapshot.forEach(doc => console.log(doc.data())). método forEach do retorno de getDocs  
 
+    const gamesLis = querySnapshot.docs.reduce((acc, doc) => {
+      const { title, developedBy, createdAt } = doc.data()
+      acc += `<li class="my-4">
+        <h5>${title}</h5>
+        
+        <ul>
+          <li>Desenvolvido por ${developedBy}</li>
+          <li>Adicionado no banco em ${createdAt.toDate()}</li>
+        </ul>
+      </li>`
+
+      return acc
+    }, '')
+
+    const gamesList = document.querySelector('[data-js="games-list"]')
+    gamesList.innerHTML = gamesLis
+  })
 

6ceda239c5906f3a06dfd72eff2040f4e4ec2e60
Alexandre Uglar <acuglar@gmail.com>
2022-03-13 18:52:08 -0300

    hide venv

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-app.js'
 import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.0.1/firebase-firestore.js'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAi8OBJqIzYxfwIHuSJFsgO5_OkAbpzegg',
-  authDomain: 'testing-firebase-60db9.firebaseapp.com',
-  projectId: 'testing-firebase-60db9',
-  storageBucket: 'testing-firebase-60db9.appspot.com',
-  messagingSenderId: '920365434979',
-  appId: '1:920365434979:web:04947e0f8dc299342ad056',
-  measurementId: 'G-Q6EWYSBD5M'
-}
+// firebaseConfig = {
+//   apiKey,
+//   authDomain,
+//   projectId,
+//   storageBucket,
+//   messagingSenderId,
+//   appId,
+//   measurementId,
+// }
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
